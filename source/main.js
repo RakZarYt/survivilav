7bb807b763c83a9ea2c992505d8aded8317a0546
RakZarYt <68829371+RakZarYt@users.noreply.github.com>
2023-11-06 12:55:09 +0300

    Крупное первое обновление
    Была разработана базовая вёрстка
    Была разработана возможность быстрого добавления статей, через js
    Был разработан первый контент для сайта

---
+++ source/main.js
@@ -0,0 +1,230 @@
+let template = [
+    {
+        "title": "Главная",
+        "article": [
+            {
+                "title": "Основная",
+                "content": `
+                <p class="heading">Добро пожаловать!</p>
+
+                <p class="text">
+                    <span class="text_yellow">Сурвивилав - это приватный проект,</span> связь двух серверов, для: 1.14.4 и 1.16.5. В связи с этим,
+                    перед попаданием на сервера и в общую телеграмм-группу, мы проводим регистрацию (кроме
+                    тех, кого пригласили другие пользователи, уже играющие).
+                </p>
+                <p class="text">
+                    <i class="i">Данные выдаются после принятия вашей заявки.</i> 
+                </p>
+                `,
+            },
+            {
+                "title": "Администрация",
+                "content": `
+                <p class="heading">От администрации</p>
+
+                <p class="text">
+                    <span class="text_yellow">Приветствую! Меня можешь звать Roman_Voran,</span> - главный администратор проекта, создатель этого веб-сайта,
+                    создатель символики сервера, а так же его спавна. Буду очень рад видеть больше приятных людей в проекте!
+                </p>
+    
+                <p class="text">
+                    <span class="text_yellow">В состав нашей администрации, входят:</span>
+                    <br> DNLL - владелец проекта
+                    <br> Roman_Voran - главный администратор проекта
+                    <br> Hell_Artem - главный модератор проекта
+                    <br> <i class="i">По всем вопросам к Roman_Voran'у.</i>
+                </p>
+    
+                <p class="text">
+                    Последним изменением в администрацтивном составе, было снятие Mvittilalka с поста администратора.
+                    Наборов в административный состав мы не проводим!
+                </p>
+                `,
+            },
+            {
+                "title": "Социальные сети",
+                "content": `
+                <p class="heading">Социальные сети</p>
+            <p class="text"><i class="i">Возможны ошибки в этой статье</i></p>
+
+            <p class="text">
+                <a href="https://t.me/+RDEAO2NJGXo2NzFi" class="link">Семья сурвивилав :D - регистрация</a> - это чат в телеграм, цель которого набрать новых людей.
+
+                <br>Семья сурвивилав :D - это основной чат проекта, цель которого: простое общение.
+                <br>
+
+                <br>Сайт проекта [<a href="https://rakzaryt.github.io/survivilav/" class="link">https://rakzaryt.github.io/survivilav/</a>] - это место, где вы можете узнать больше информации по нашему проекту.
+
+                <br>Survivilav News - наш тик-ток аккаунт, который на данный момент не активен.
+
+                <br>Survivilav - наш дискорд канал, на данный момент не активен.
+
+                <br>SURVIVILAV - наш канал на ютубе, на данный момент не активен.
+                <br>
+
+                <br>Все видеоролики, по нашему серверу, представлены ниже (видеоролики, могут не иметь отношения к авторам проекта):
+                <br><a href="https://www.youtube.com/watch?v=BGi2cuHCkUw" class="link">https://www.youtube.com/watch?v=BGi2cuHCkUw</a>
+                <br><a href="https://www.youtube.com/watch?v=MKi5O_23AXw" class="link">https://www.youtube.com/watch?v=MKi5O_23AXw</a>
+                <br><a href="https://www.youtube.com/watch?v=imBSJjMM5tI&t" class="link">https://www.youtube.com/watch?v=imBSJjMM5tI&t</a>
+                <br><a href="https://www.youtube.com/watch?v=UgxMXylgIAo" class="link">https://www.youtube.com/watch?v=UgxMXylgIAo</a>
+                <br><a href="https://www.youtube.com/watch?v=YcSKPduX4iY" class="link">https://www.youtube.com/watch?v=YcSKPduX4iY</a>
+                <br>Будем рады добавить, ваше видео в данный список (Добавляем/удаляем по просьбе).
+            </p>
+                `,
+            },
+            {
+                "title": "Правила проекта",
+                "content": `
+                <p class="heading">Правила проекта</p>
+            <p class="text">
+                <br>ЮИграя на сервере вы автоматически соглашаетесь со всеми правилами.
+                <br>На все правила есть исключения, обращайтесь к составу высшей администрации или модерации указанной ниже.
+                Правила написаны: 
+                <br>
+                <br><span class="text_yellow">Broadcast [Bct] –</span> Запрещено рекламировать сторонние проекты. (Бан - навсегда минут)
+                <br>
+                <br><span class="text_yellow">ServerGay [SG] –</span> Запрещено оскорблять сервер (Бан - навсегда минут)
+                <br>
+                <br><span class="text_yellow">MassViolation [MV] –</span> При нарушении 2-ух, и более правил, все нарушения начинают суммироваться в бан. (Бан - 60/14400 минут)
+                <br>
+                <br><span class="text_yellow">Bug Abuse –</span> Запрещено использовать баги сервера в целях собственной выгоды. (Бан - 60/43200 минут)
+                <br>
+                <br><span class="text_yellow">Chetas –</span> Запрещено использовать стороннее П.О которое может дать преимущество перед другими игроками. (Бан - 60/43200 минут)
+                Правила добросовестной игры на Survivilav
+                <br>
+                <br><span class="text_yellow">Toxic [Tx] –</span> Игрокам запрещено неуважительно себя вести, по отношению к остальным. (Мут/Гаг/Джаил - 4/1440 минут)
+                <br>
+                <br><span class="text_yellow">ChatToxic [CT] –</span> Запрещено грубить/оскорблять в чате. (Мут - 10/30 минут)
+                <br>
+                <br><span class="text_yellow">BadWords [BW] –</span> Запрещено писать какие-либо плохие слова в чате. (по усмотрению администратора) (Мут - 5/60 минут)
+                Правила администрирования на Survivilav
+                <br>
+                <br><span class="text_yellow">NoGame [NG] –</span> Запрещено закрывать глаза на нарушения (искл.Высшая администрация). (Бан/Джаил - 4/1440 минут) 
+                - Не юзабельно -
+                <br>
+                <br><span class="text_yellow">NonRP [NRP] –</span> Запрещено подталкивать игроков на нарушения действующих правил. (Бан/Джаил - 4/1440 минут)
+                <br>
+                <br><span class="text_yellow">Neadekvat [NT] –</span> Категорически запрещено себя неадеватно вести на посту администратора/модератора (маты, оскорбления, по усмотрению администрации). (Снятие с поста администратора)
+            </p>
+            <p class="text">
+                <b class="b">Правила от: DNLL</b>
+            </p>
+                `,
+            },
+            {
+                "title": "Контент и новости",
+                "content": `
+                <p class="heading">Интересные изображения:</p>
+                <div class="content__table-picture">
+                    <img src="source/picture/article/content and news/home.png" alt="" class="picture picture_content">
+                    <img src="source/picture/article/content and news/spawn.png" alt="" class="picture picture_content">
+                    <img src="source/picture/article/content and news/storehouse.png" alt="" class="picture picture_content">
+                    <img src="source/picture/article/content and news/bed.png" alt="" class="picture picture_content">
+                    <img src="source/picture/article/content and news/market.png" alt="" class="picture picture_content">
+                    <img src="source/picture/article/content and news/other bed.png" alt="" class="picture picture_content">
+                    <img src="source/picture/article/content and news/rainbow.png" alt="" class="picture picture_content">
+                    <img src="source/picture/article/content and news/ship.png" alt="" class="picture picture_content">
+                    <img src="source/picture/article/content and news/title.png" alt="" class="picture picture_content">
+                </div>
+                `,
+            },
+            {
+                "title": "Обновления",
+                "content": `
+                <p class="heading">Последнее обновление проекта:</p>
+                <p class="text"><b class="b">Был разработан новый веб-сайт.</b> - 06.11.2023</p>
+                `,
+            },
+        ],
+    },
+    {
+        "title": "Проекты",
+        "article": [
+            {
+                "title": "Сервер 1.16.5",
+                "content": `
+                <p class="heading">Основной сервер</p>
+                <p class="text">Сурвивилав 1.16.5 - это основной сервер проекта, здесь установлены плагины, на: простые команды типа /home, /warp и так далее. Помимо этого здесь, у большинства присутствуют моды: EmotiCraft, PlasmoVoice, Kotlin.</p>
+                `,
+            },
+            {
+                "title": "Сервер 1.14.4",
+                "content": `
+                <p class="heading">Дополнительный сервер</p>
+                <p class="text">Сурвивилав 1.14.4 - это дополнительный (более старший товарищ), здесь установлены плагины, на: приваты, базовые команды, и так далее. Нет никаких модов.</p>
+                `,
+            },
+        ],
+    },
+];
+
+let content = document.querySelector (".content");
+let navSubtitle = document.querySelector (".nav-subtitle");
+let navTitle = document.querySelector (".nav-title");
+
+// Функция вывода кнопок сверху
+function viewButtonTop () {
+    let viewButtonTopInner = '<img src="source/picture/icon.png" alt="логотип сайта" class="picture logo">';
+    for (templateElement in template) {
+        viewButtonTopInner = viewButtonTopInner + `
+        <button class="button" onclick="viewButtonLeft ('${template[templateElement]["title"]}'); contentClear ();">
+                <p class="text">${template[templateElement]["title"]}</p>
+        </button>
+        `;
+    };
+
+    navTitle.innerHTML = viewButtonTopInner;
+    return viewButtonTopInner;
+};
+
+// Функция вывода кнопок слева
+function viewButtonLeft (value = "Главная") {
+    let viewButtonLeftInner = '';
+    for (templateParentElement in template) {
+        if (template[templateParentElement]["title"] === value) {
+            templateArticles = template[templateParentElement]["article"];
+            for (templateElement in templateArticles) {
+                viewButtonLeftInner = viewButtonLeftInner + `
+                <button class="button nav-subtitle__button" onclick="viewContent ('${value}', '${templateArticles[templateElement]['title']}')">
+                        <p class="text">${templateArticles[templateElement]["title"]}</p>
+                </button>
+                `;
+            };
+        };
+    };
+
+    navSubtitle.innerHTML = viewButtonLeftInner;
+    return viewButtonLeftInner;
+};
+
+function viewContent (valueTop = "Главная", valueLeft = "") {
+    let viewContentInner = '';
+    for (templateParentElement in template) {
+        if (template[templateParentElement]["title"] === valueTop) {
+            templateArticles = template[templateParentElement]["article"];
+            for (templateElement in templateArticles) {
+                if (templateArticles[templateElement]["title"] === valueLeft) {
+                    viewContentInner = viewContentInner + `
+                        ${templateArticles[templateElement]["content"]}
+                    `;
+                }
+            };
+        };
+    };
+
+    content.innerHTML = viewContentInner;
+    return viewContentInner;
+}
+
+function contentClear () {
+    content.innerHTML = "";
+}
+
+function start () {
+    viewButtonTop ()
+    viewButtonLeft ("Главная")
+    viewContent ("Главная", "Основная")
+    console.log ("Готово");
+}
+
+start ();
